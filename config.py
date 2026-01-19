6bbe55034b7b0d4d6877407cd20d430fa29a2001
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-01-20 00:10:40 +0530

    Clear sensitive data from config.py
    Removed sensitive information such as BOT_TOKEN, URL_A, and MONGO_URI from config.py.

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 
 API_ID = 29245477 #API ID get it from telegram.org
 API_HASH = "0abc83883262245c90ca337b7a0375c4" #API HASH get it from telegram.org
-BOT_TOKEN = "8388670314:AAGxN6uPVmcI2r3RnNSaFcc78E7Le2i8uoE" #Bot Token get it from @BotFather
-URL_A = "https://animeuknews.net/feed/" #News RSS Feed Url 2
+BOT_TOKEN = "" #Bot Token get it from @BotFather
+URL_A = "" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
 START_PIC = "" #Startup Pic [Optional]
-MONGO_URI = "mongodb+srv://nier88881:8qLsalGiPAvbwKTP@cluster0.od2tq6h.mongodb.net/" #Databse uri get it from https://www.mongodb.com/
+MONGO_URI = "" #Databse uri get it from https://www.mongodb.com/
 ADMINS = [8226767954]
