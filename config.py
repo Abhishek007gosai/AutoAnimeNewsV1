fc02e5da10385468ea3e6c636aab533c7dfbbb53
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-01-19 19:48:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
-API_ID = "29245477" #API ID get it from telegram.org
-API_HASH = "0abc83883262245c90ca337b7a0375c4"
-BOT_TOKEN = "8388670314:AAGxN6uPVmcI2r3RnNSaFcc78E7Le2i8uoE"
-URL_A = "https://myanimelist.net/rss/news.xml"
-#URL_B = ""
-START_PIC = "https://litter.catbox.moe/m4c4wu.jpg" #Startup Pic
-MONGO_URI = "mongodb+srv://nier88881:8qLsalGiPAvbwKTP@cluster0.od2tq6h.mongodb.net/" #Databse uri get it from https://www.mongodb.com/)
+API_ID = 29245477 #API ID get it from telegram.org
+API_HASH = "0abc83883262245c90ca337b7a0375c4" #API HASH get it from telegram.org
+BOT_TOKEN = "8388670314:AAGxN6uPVmcI2r3RnNSaFcc78E7Le2i8uoE" #Bot Token get it from @BotFather
+URL_A = "https://myanimelist.net/rss/news.xml" #News RSS Feed Url 2
+#URL_B = "" #News RSS Feed Url 2
+START_PIC = "https://files.catbox.moe/7iqqsb.jpg" #Startup Pic [Optional]
+MONGO_URI = "mongodb+srv://nier88881:8qLsalGiPAvbwKTP@cluster0.od2tq6h.mongodb.net/" #Databse uri get it from https://www.mongodb.com/
 ADMINS = [8226767954]
