1184ca4583617b0e8ecfa13c0bc41e96cc763c9e
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-01-19 19:10:49 +0530

    Change BOT_TOKEN in config.py
    Updated the BOT_TOKEN for the Telegram bot.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 API_ID = "29245477" #API ID get it from telegram.org
 API_HASH = "0abc83883262245c90ca337b7a0375c4"
-BOT_TOKEN = "8159731963:AAHflPblpC3mnTr1Jg907NXtMJYmVHWDn_g"
+BOT_TOKEN = "8388670314:AAHXEO6NnwlExmXiauq78IYbUp8LyZQylFQ"
 URL_A = "https://myanimelist.net/rss/news.xml"
 #URL_B = ""
 START_PIC = "https://litter.catbox.moe/m4c4wu.jpg" #Startup Pic
