336a6e65d230cb2f37703ac1cd9bd170b1909c32
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-01-19 23:18:48 +0530

    Set BOT_TOKEN in config.py
    Added BOT_TOKEN for Telegram bot integration.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 API_ID = 29245477 #API ID get it from telegram.org
 API_HASH = "0abc83883262245c90ca337b7a0375c4" #API HASH get it from telegram.org
-BOT_TOKEN = "" #Bot Token get it from @BotFather
+BOT_TOKEN = "8388670314:AAGxN6uPVmcI2r3RnNSaFcc78E7Le2i8uoE" #Bot Token get it from @BotFather
 URL_A = "https://myanimelist.net/rss/news.xml" #News RSS Feed Url 2
 URL_B = "https://www.theanimedaily.com/feed/" #News RSS Feed Url 2
 START_PIC = "https://www.animenewsnetwork.com/" #Startup Pic [Optional]
