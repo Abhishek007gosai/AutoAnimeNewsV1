b5febb382e76b54c2807fb2ca6d321901cf6c409
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-04-01 02:58:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 API_ID = 29245477 #API ID get it from telegram.org
 API_HASH = "0abc83883262245c90ca337b7a0375c4" #API HASH get it from telegram.org
-BOT_TOKEN = "" #Bot Token get it from @BotFather
+BOT_TOKEN = "8695282197:AAHtLMueQLaxKlZA7RPEO-vZbyvD7Foxjzs" #Bot Token get it from @BotFather
 URL_A = "" #News RSS Feed Url 2
 #URL_B = "" #News RSS Feed Url 2
 START_PIC = "" #Startup Pic [Optional]
